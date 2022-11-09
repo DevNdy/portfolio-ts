7f21c4b4714f4f250b72bdbc7c1ebdc79c4e4e45
DevNdy <developpeur.ndy@gmail.com>
2022-11-09 12:10:41 +0100

    F02 change img skill

--- src/text/skillImg.ts
+++ src/text/skillImg.ts
@@ -4,19 +4,20 @@ export const imgTech = [
   "https://dyma.fr/assets/technos/htmlcss-logo-title.svg",
   "https://www.seekpng.com/png/full/377-3772047_sass-logo.png",
   "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png",
-  "https://brandlogos.net/wp-content/uploads/2021/12/flutter-brandlogo.net_.png",
-  "https://www.silicon.fr/wp-content/uploads/2018/08/Firebase.png",
+  "https://purecode.sa/wp-content/uploads/flutter-logo-5086DD11C5-seeklogo.com_-e1615739022704.png",
+  "https://assets.stickpng.com/images/5847f40ecef1014c0b5e488a.png",
   "https://miro.medium.com/max/400/1*b7DwaOEfreG253K8Z_QzkA.png",
 ];
 
 export const imgDesign = [
-  "https://seeklogo.com/images/F/figma-logo-6F8EFD765C-seeklogo.com.png",
+  "https://cdn-icons-png.flaticon.com/512/5968/5968705.png",
   "https://cdn-ffkam.nitrocdn.com/XuXgiNJlPPKxCCYEgHDCJxnscwsOEvRm/assets/static/optimized/rev-c42af46/wp-content/uploads/2019/10/icone-adobe-xd.png",
+  "https://cdn-icons-png.flaticon.com/512/220/220238.png",
   "https://ph-files.imgix.net/25b07021-eab0-4596-b444-0fcc41031595.png?auto=format",
 ];
 
 export const imgOther = [
-  "https://camo.githubusercontent.com/a8bf3da6a2b756488b26692367a858ebbb4b5d77584c2fd6354fcafa7d93fd03/687474703a2f2f6769742d73636d2e636f6d2f696d616765732f6c6f676f732f646f776e6c6f6164732f4769742d4c6f676f2d32436f6c6f722e706e67",
+  "https://git-scm.com/images/logos/downloads/Git-Icon-Black.png",
   "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/2048px-Visual_Studio_Code_1.35_icon.svg.png",
   "https://www.pngkey.com/png/full/207-2078764_andro-apple-windows-icon-android-ios-windows-icon.png",
 ];
